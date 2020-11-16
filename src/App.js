b14d00d441161606d921392588151558ea21f257
fallore <nicollecanuton@gmail.com>
2020-11-16 18:45:28 -0300

    faleconosco desktop iniciado

--- src/App.js
+++ src/App.js
@@ -2,15 +2,18 @@ import React from 'react';
 import FooterJS from './components/footer/footer.js';
 import HeaderJS from './components/header/header.js';
 import QuemSomosJS from './components/quemsomos/quemsomos.js';
+import FaleConoscoJS from './components/faleconosco/faleconosco.js';
 import HomeJS from './components/home/home.js';
+
 
 function App() {
   return (
     <div className="App">
 	    <div>
         <HeaderJS/>
-        {/*<HomeJS/>*/}
+        <HomeJS/>
         <QuemSomosJS/>
+        <FaleConoscoJS/>
 	      <FooterJS/>  
 	    </div>
     </div>
