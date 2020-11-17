15a3134557223adbcba5370b0ef40f0d9a75fd13
Fabio Henrique <fabiohenrique6@hotmail.com>
2020-11-17 15:17:28 -0300

    nossos servicos, seção completa e responsiva

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import FooterJS from './components/footer/footer.js';
 import Header from './components/header/header.js';
 import QuemSomosJS from './components/quemsomos/quemsomos.js';
+import Servicos from './components/servicos/servicos.js';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
 	    <div>
         <Header/>
         <QuemSomosJS/>
-	    <FooterJS/>  
+        <Servicos />
+	      <FooterJS/>
 	    </div>
     </div>
   );
---
+++ src/components/servicos/servicos.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './styles.css';
+import '../.././globalStyle.css';
+
+import websites from '../.././assets/images/websiteicon.svg';
+import aplicativos from '../.././assets/images/appicon.svg';
+import sistemas from '../.././assets/images/sistemasicon.svg';
+import consultoria from '../.././assets/images/consultoriaicon.svg';
+
+
+export default function Servicos() {
+    return (
+        <div id = "servicos">
+            <div className = "titulo-servicos">
+                <h1>Nossos Serviços</h1>
+            </div>
+            <div className = "info-servicos">
+                <div>
+                    <img src = {websites} />
+                    <h2>Websites</h2>
+                    <p>Desenvolvemos websites personalizados, utilizando das mais <b>novas tecnologias</b> do mercado.</p>
+                </div>
+
+                <div>
+                    <img src = {aplicativos} />
+                    <h2>Aplicativos</h2>
+                    <p>Desenvolvemos para as plataformas <b>ANDROID</b> e <b>IOS</b>, de forma totalmente personalizada.</p>
+                </div>
+
+                <div>
+                    <img src = {sistemas} />
+                    <h2>Sistemas de Gerenciamento</h2>
+                    <p>Criamos sistemas de gerenciamento visando principalmente <b>otimizar</b> o seu negócio.</p>
+                </div>
+
+                <div>
+                    <img src = {consultoria} />
+                    <h2>Consultorias</h2>
+                    <p>Prestamos consultorias voltadas para <b>soluções</b> e <b>manutenções</b> de sistemas digitais.</p>
+                </div>
+
+            </div>
+
+        </div>
+
+    );
+}
